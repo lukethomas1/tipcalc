5ffe3fc1f4185f5dbd3f8869b06911bc1d2f2a82
Luke T Thomas <l2thomas@its-gl1w-mac10.ucsd.edu>
2016-11-28 13:38:06 -0800

    first commit, program should be working but i couldnt test

--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var plusSign: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
-
+    @IBAction func calcTip(sender: AnyObject) {
+        let tipArray = [0.15, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = tipArray[tipPercent.selectedSegmentIndex]
+        
+        billLabel.text = String(format: "$%.2f", bill)
+        tipLabel.text = String(format: "$%.2f", bill * tip)
+        totalLabel.text = String(format: "$%.2f", bill * tip + bill)
+    }
 }
 
