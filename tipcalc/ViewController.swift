724ef9d7e2b6b000c5d1f0dbf2739a272d414152
Luke T Thomas <l2thomas@its-gl1w-mac10.ucsd.edu>
2016-11-28 12:10:03 -0800

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Luke T Thomas on 11/28/16.
+//  Copyright © 2016 Luke T Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
